5d7bad10f4805c17e39e0b096836419d5a3ac371
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-09-30 10:52:25 +0700

    update

--- install-mongo.sh
+++ install-mongo.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-ls \
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add - \
 && \
-ls -la
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list \
+&& \
+sudo apt-get update -y \
+&& \
+sudo apt-get install -y mongodb-org \
+&& \
+sudo systemctl start mongod \
+&& \
+sudo systemctl enable mongod \
+&& \
+sudo systemctl status mongod
+
+# run script: 
+# bash <(curl -s https://raw.githubusercontent.com/nguyenleminhdev/linux-script/master/install-mongo.sh)
