6c587064eb83a6fae43cac7c44bbec53da9e7118
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-09-30 14:17:09 +0700

    update

---
+++ install-nodejs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+curl -sL https://deb.nodesource.com/setup_17.x -o /tmp/nodesource_setup.sh \
+&& \
+sudo bash /tmp/nodesource_setup.sh \
+&& \
+sudo apt install nodejs \
+&& \
+sudo npm i -g pm2 \
+&& \
+pm2 install pm2-logrotate \
+&& \
+node -v \
+&& \
+npm -v \
+&& \
+pm2 ls
+
+# run script: 
+# bash <(curl -s https://raw.githubusercontent.com/nguyenleminhdev/linux-script/master/install-nodejs.sh)
