eac95b668c83168879b7dbbfb809369c53969c20
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-09-30 11:12:30 +0700

    update

--- install-redis.sh
+++ install-redis.sh
@@ -3,3 +3,6 @@
 sudo apt update -y \
 && \
 sudo apt install redis-server -y
+
+# run script: 
+# bash <(curl -s https://raw.githubusercontent.com/nguyenleminhdev/linux-script/master/install-redis.sh)
