ee2e8b8680dcb91d7d31c0c69abc8db738035179
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-09-30 11:42:54 +0700

    update

---
+++ install-es.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg \
+&& \
+sudo apt-get install apt-transport-https -y \
+&& \
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list \
+&& \
+sudo apt-get update -y \
+&& \
+sudo apt-get install elasticsearch  -y \
+&& \
+sudo apt-get install kibana -y \
+&& \
+sudo /bin/systemctl daemon-reload \
+&& \
+sudo /bin/systemctl enable elasticsearch.service \
+&& \
+sudo systemctl start elasticsearch.service \
+&& \
+sudo /bin/systemctl enable kibana.service \
+&& \
+sudo systemctl start kibana.service \
+&& \
+sudo systemctl status elasticsearch.service \
+&& \
+sudo systemctl status kibana.service \
+&& \
+curl -XGET http://localhost:9200 \
+&& \
+curl -XGET http://localhost:5601 \
+
+# run script: 
+# bash <(curl -s https://raw.githubusercontent.com/nguyenleminhdev/linux-script/master/install-es.sh)
